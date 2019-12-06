b54910a11b8d9b78288f764435d45145cfa9fa53
Chennan <chennancd@gmail.com>
2019-12-06 18:57:59 +0800

    Create Game.java

---
+++ src/com/north/application/Game.java
@@ -0,0 +1,10 @@
+package com.north.application;
+
+import com.north.gamecore.Board;
+
+public class Game {
+	public static void main(String args[]) {
+		Board board = new Board();
+		board.startNewGameOfPVE();
+	}
+}
