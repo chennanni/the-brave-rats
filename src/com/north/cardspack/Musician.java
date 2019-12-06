fd76dade264f85ddd23145b71df885fb1513e57f
Chennan <chennancd@gmail.com>
2019-12-06 18:59:20 +0800

    Update Musician.java

--- src/com/north/cardspack/Musician.java
+++ src/com/north/cardspack/Musician.java
@@ -1,18 +1,20 @@
 package com.north.cardspack;
 
+import com.north.gamecore.Card;
+
 public class Musician extends Card {
 	private final int NUMBER = 0;
 	private final String NAME = "Musician";
 	private final String DESCRIPTION ="This round is on hold.";
-	
-	Musician() {
+
+	public Musician() {
 		super.setNumber(this.NUMBER);
 		super.setName(this.NAME);
 		super.setDescription(this.DESCRIPTION);
 	}
 	
 	public void magic() {
-		//TODO
-	};
+		// 0 – Musician: This round is on hold.
+	}
 
 }
