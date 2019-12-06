e318914558ca18b9070f4591296177ad504ebec0
Chennan <chennancd@gmail.com>
2019-12-06 18:58:37 +0800

    Update Assassin.java

--- src/com/north/cardspack/Assassin.java
+++ src/com/north/cardspack/Assassin.java
@@ -1,18 +1,20 @@
 package com.north.cardspack;
 
+import com.north.gamecore.Card;
+
 public class Assassin extends Card {
 	private final int NUMBER = 3;
 	private final String NAME = "Assassin";
 	private final String DESCRIPTION ="The lower strength wins instead.";
 
-	Assassin() {
+	public Assassin() {
 		super.setNumber(NUMBER);
 		super.setName(NAME);
 		super.setDescription(DESCRIPTION);
 	}
 	
 	public void magic() {
-		//TODO
+		// 3 – Assassin: The lower strength wins instead.
 	};
 
 }
