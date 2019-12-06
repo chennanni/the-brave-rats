39937ee967406076d80a8686673fb297d763b48b
Chennan <chennancd@gmail.com>
2019-12-06 18:59:44 +0800

    Update Princess.java

--- src/com/north/cardspack/Princess.java
+++ src/com/north/cardspack/Princess.java
@@ -1,18 +1,20 @@
 package com.north.cardspack;
 
+import com.north.gamecore.Card;
+
 public class Princess extends Card {
 	private final int NUMBER = 1;
 	private final String NAME = "Princess";
 	private final String DESCRIPTION ="If your opponent plays the Prince you win the whole game.";
-	
-	Princess() {
+
+	public Princess() {
 		super.setNumber(this.NUMBER);
 		super.setName(this.NAME);
 		super.setDescription(this.DESCRIPTION);
 	}
 	
 	public void magic() {
-		//TODO
+		// 1 – Princess: If your opponent plays the Prince you win the whole game.
 	};
 
 }
