c51edcf4585ec4c736e1dbda62b92d04a3d10858
Chennan <chennancd@gmail.com>
2019-12-06 18:59:54 +0800

    Update Spy.java

--- src/com/north/cardspack/Spy.java
+++ src/com/north/cardspack/Spy.java
@@ -1,18 +1,20 @@
 package com.north.cardspack;
 
-public class Spy extends Card {
-	private final int NUMBER = 2;
-	private final String NAME = "Spy";
-	private final String DESCRIPTION ="Next round, you choose a card after your opponent reveals theirs.";
-	
-	Spy() {
+import com.north.gamecore.Card;
+
+public class Princess extends Card {
+	private final int NUMBER = 1;
+	private final String NAME = "Princess";
+	private final String DESCRIPTION ="If your opponent plays the Prince you win the whole game.";
+
+	public Princess() {
 		super.setNumber(this.NUMBER);
 		super.setName(this.NAME);
 		super.setDescription(this.DESCRIPTION);
 	}
 	
 	public void magic() {
-		//TODO
+		// 1 – Princess: If your opponent plays the Prince you win the whole game.
 	};
-	
+
 }
