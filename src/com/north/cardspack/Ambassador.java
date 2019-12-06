9194c49915ee0a0e3fb1e22242d6fbc505ad89e3
Chennan <chennancd@gmail.com>
2019-12-06 18:58:17 +0800

    Update Ambassador.java

--- src/com/north/cardspack/Ambassador.java
+++ src/com/north/cardspack/Ambassador.java
@@ -1,17 +1,19 @@
 package com.north.cardspack;
 
+import com.north.gamecore.Card;
+
 public class Ambassador extends Card {
 	private final int NUMBER = 4;
 	private final String NAME = "Ambassador";
 	private final String DESCRIPTION ="If you win, it counts as 2 rounds.";
 	
-	Ambassador() {
+	public Ambassador() {
 		super.setNumber(NUMBER);
 		super.setName(NAME);
 		super.setDescription(DESCRIPTION);
 	}
 
 	public void magic() {
-		System.out.println("I'm a Ambassador, magic!");
+		// 4 – Ambassador: If you win, it counts as 2 rounds.
 	}	
 }
