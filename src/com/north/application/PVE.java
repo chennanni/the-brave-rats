9ef1bbd65def4b6443b283560c5c7359c9565077
Chennan <chennancd@gmail.com>
2019-12-12 14:04:49 +0800

    Create PVE.java

---
+++ src/com/north/application/PVE.java
@@ -0,0 +1,10 @@
+package com.north.application;
+
+import com.north.gamecore.Board;
+
+public class PVE {
+    public static void main(String args[]) {
+        Board board = new Board();
+        board.startNewGameOfPVE();
+    }
+}
