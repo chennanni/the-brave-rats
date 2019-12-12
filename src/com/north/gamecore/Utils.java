f0cab793415149d6cefaa970fcff81b45bbb5885
Chennan <chennancd@gmail.com>
2019-12-12 14:09:38 +0800

    Update Utils.java

--- src/com/north/gamecore/Utils.java
+++ src/com/north/gamecore/Utils.java
@@ -2,6 +2,71 @@ package com.north.gamecore;
 
 public class Utils {
 
+    public static boolean parseOptionSelection(int num, Player player, String history) {
+        boolean showOptionMenu = true;
+        switch(num) {
+            case 1:
+                // 1: ready to play
+                showOptionMenu = false;
+                break;
+            case 2:
+                // 2: show my cards in hand
+                player.printNotUsedCards();
+                break;
+            case 3:
+                // 3: show my cards played
+                player.printUsedCards();
+                break;
+            case 4:
+                // 4: show opponent's cards played
+                player.getOpponent().printExposedCards();
+                break;
+            case 5:
+                // 5: show the score board
+                UserInterface.clearConsole();
+                UserInterface.printScoreBoard(player, player.getOpponent());
+                break;
+            case 6:
+                // 6: show the battle history
+                UserInterface.printBattleHistory(history);
+                break;
+            default:
+                UserInterface.print("Input err, please choose from 1-7!");
+                sleep(1);
+                UserInterface.clearConsole();
+        }
+        return showOptionMenu;
+    }
+
+    /**
+     * given a card num, return the card if available
+     * @param num
+     * @param player
+     * @return
+     */
+    public static Card parseCardSelection(int num, Player player, Deck CommonDeck) {
+        Card playersCard = null;
+        if (num>=0 && num<=7) {
+            playersCard = CommonDeck.getCards().get(num);
+            if (player instanceof AI || player.haveThisCard(num)) { // assume AI will always play right
+                return playersCard;
+            } else {
+                UserInterface.println("Input err, please choose a NOT USED card!");
+                sleep(1);
+                return null;
+            }
+        } else {
+            UserInterface.println("Input err, please choose from 0-7!");
+            sleep(1);
+            return null;
+        }
+    }
+
+    /**
+     * parse input String to number, catch exception
+     * @param input
+     * @return
+     */
     public static int parseInput(String input) {
         int num = -1;
         try {
@@ -12,6 +77,10 @@ public class Utils {
         return num;
     }
 
+    /**
+     * sleep for some time, printing .
+     * @param second
+     */
     public static void sleep(int second) {
         try {
             for (int i=0; i<second; i++){
