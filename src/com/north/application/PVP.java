1ee3440324fabb52fff7a4ad0b1afb25454c9f16
Chennan <chennancd@gmail.com>
2019-12-12 14:05:00 +0800

    Create PVP.java

---
+++ src/com/north/application/PVP.java
@@ -0,0 +1,10 @@
+package com.north.application;
+
+import com.north.gamecore.Board;
+
+public class PVP {
+    public static void main(String args[]) {
+        Board board = new Board();
+        board.startNewGameOfPVP();
+    }
+}
